bcd8266f6b8950d94cc25d490e48f3a0e40d0654
dht <603928588@qq.com>
2023-06-13 10:52:35 +0800

    麻煩

--- play/src/views/color/index.vue
+++ play/src/views/color/index.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
     <div class="main-color">
-      <template v-for="item in mainColor" :key="item">
-        <div class="item-color" :style="{ background: getCssVar(item) }">{{ item }}</div>
+      <template v-for="item in mainColor" :key="item.cssVar">
+        <div class="item-color" :style="{ background: getCssVar(item.cssVar) }">{{ item.cssVar + item.color }}</div>
       </template>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { config } from '@beaver-ui/theme/config'
 const { namespace, connector } = config
 
 function getCssVar(name: string) {
   return `var(${name})`
 }
+function getCssVarVal(cssVar: string) {
+  return getComputedStyle(document.documentElement).getPropertyValue(cssVar)
+}
 
 const mainColor = ref(
-  [...Array(20)].map((li, index) => {
-    return `--${namespace}${connector}main-color${index}`
+  [...Array(10)].map((li, index) => {
+    const cssVar = `--${namespace}${connector}main-color${index}`
+
+    return {
+      cssVar,
+      color: getCssVarVal(cssVar),
+    }
   }),
 )
 </script>
