5b6a273897d872c6046edd99aa4c014bec3346a0
dht <603928588@qq.com>
2023-07-24 10:01:04 +0800

    css滚动条实验

--- play/src/views/PageScrollingProgress/index.vue
+++ play/src/views/PageScrollingProgress/index.vue
@@ -1,40 +1,49 @@
 <template>
   <div>
-    <a href="https://www.baidu.com?test =1" target="_blank">百度，点击查看效果</a>
+    12313131
+    <!--    <div class="page-scrolling-progress"></div>-->
+    <div class="test"></div>
+    1231311
   </div>
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue'
-/*
-思路：
-1、拦截所有的 a 标签的点击事件实现方式
-2、遍历范围内所有的 a 标签，通过正则的方式匹配地址进行替换
-*/
-document.body.addEventListener('click', function (event) {
-  event.preventDefault() // 先阻止默认行为
-  const href = (event.target as HTMLAnchorElement).href
-  if (!href) return
-  // 安全地址检测
-  const safeUrlList = [
-    // 'baidu.com', 'google.com' 示例
-  ]
-  // some如果有一个满足条件就返回true
-  const isSafeUrl = safeUrlList.some((urlItem) => {
-    return href.includes(urlItem)
-  })
 
-  let ATag = document.createElement('a')
-  ATag.target = '_blank' // 统一新窗口打开
-  if (isSafeUrl) {
-    // 是安全地址，就不拦截
-    ATag.href = href
-  } else {
-    // 不是安全地址，就拦截
-    ATag.href = '你的网址拦截页面地址?outerChain=' + href
-  }
-  ATag.click()
-  ATag = null // 释放内存
+onMounted(() => {
+  //
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style>
+body {
+  background-image: linear-gradient(to right top, #ffcc00 50%, #eee 50%);
+  background-size: 100% calc(100% - 100vh + 5px);
+  background-repeat: no-repeat;
+}
+body::after {
+  content: '';
+  position: fixed;
+  top: 5px;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #ffffff;
+  z-index: -1;
+}
+</style>
+<style scoped lang="scss">
+.page-scrolling-progress {
+  position: fixed;
+  top: 0;
+  width: 100px;
+  background: red;
+  height: 5px;
+  z-index: 999;
+  transition: width 0.3s ease-in-out;
+}
+
+.test {
+  width: 1px;
+  height: 200vh;
+}
+</style>
