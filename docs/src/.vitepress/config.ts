6ffca7d59a878852cda7ea37d5d2c59506a1c0a8
dht <603928588@qq.com>
2023-09-04 17:40:47 +0800

    一些基本配置

--- docs/src/.vitepress/config.ts
+++ docs/src/.vitepress/config.ts
@@ -4,4 +4,33 @@ export default defineConfig({
   title: 'beaver',
   titleTemplate: ':title | beaver',
   outDir: '../dist',
+  head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
+  lastUpdated: true,
+  themeConfig: {
+    logo: '/images/logo.png',
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/ht-sauce' },
+      { icon: 'discord', link: 'https://juejin.cn/user/1873223545523101' },
+    ],
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      {
+        text: 'Dropdown Menu',
+        items: [
+          { text: 'Item A', link: '/item-1' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' },
+        ],
+      },
+    ],
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          { text: 'Introduction', link: '/introduction' },
+          { text: 'Getting Started', link: '/getting-started' },
+        ],
+      },
+    ],
+  },
 })
