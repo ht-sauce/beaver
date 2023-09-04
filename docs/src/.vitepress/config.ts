6ba53b142ecfb0b0dfd5eacfe62cd93e5600eebf
dht <603928588@qq.com>
2023-09-04 17:42:52 +0800

    更新配置

--- docs/src/.vitepress/config.ts
+++ docs/src/.vitepress/config.ts
@@ -13,24 +13,24 @@ export default defineConfig({
       { icon: 'discord', link: 'https://juejin.cn/user/1873223545523101' },
     ],
     nav: [
-      { text: 'Guide', link: '/guide' },
-      {
-        text: 'Dropdown Menu',
-        items: [
-          { text: 'Item A', link: '/item-1' },
-          { text: 'Item B', link: '/item-2' },
-          { text: 'Item C', link: '/item-3' },
-        ],
-      },
+      // { text: 'Guide', link: '/guide' },
+      // {
+      //   text: 'Dropdown Menu',
+      //   items: [
+      //     { text: 'Item A', link: '/item-1' },
+      //     { text: 'Item B', link: '/item-2' },
+      //     { text: 'Item C', link: '/item-3' },
+      //   ],
+      // },
     ],
-    sidebar: [
-      {
-        text: 'Guide',
-        items: [
-          { text: 'Introduction', link: '/introduction' },
-          { text: 'Getting Started', link: '/getting-started' },
-        ],
-      },
-    ],
+    // sidebar: [
+    //   {
+    //     text: 'Guide',
+    //     items: [
+    //       { text: 'Introduction', link: '/introduction' },
+    //       { text: 'Getting Started', link: '/getting-started' },
+    //     ],
+    //   },
+    // ],
   },
 })
