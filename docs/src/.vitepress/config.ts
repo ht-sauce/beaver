f1791b7739c44829b6df379dc5cd9c9a7e98c846
dht <603928588@qq.com>
2023-09-04 18:19:46 +0800

    明天开始新尝试

--- docs/src/.vitepress/config.ts
+++ docs/src/.vitepress/config.ts
@@ -4,10 +4,30 @@ export default defineConfig({
   title: 'beaver',
   titleTemplate: ':title | beaver',
   outDir: '../dist',
+  lang: 'zh-CN',
   head: [['link', { rel: 'icon', href: '/images/favicon.ico' }]],
   lastUpdated: true,
   themeConfig: {
     logo: '/images/logo.png',
+    search: {
+      provider: 'local',
+      options: {
+        translations: {
+          button: {
+            buttonText: '搜索文档',
+            buttonAriaLabel: '搜索文档',
+          },
+          modal: {
+            noResultsText: '无法找到相关结果',
+            resetButtonTitle: '清除查询条件',
+            footer: {
+              selectText: '选择',
+              navigateText: '切换',
+            },
+          },
+        },
+      },
+    },
     socialLinks: [
       { icon: 'github', link: 'https://github.com/ht-sauce' },
       { icon: 'discord', link: 'https://juejin.cn/user/1873223545523101' },
@@ -23,14 +43,12 @@ export default defineConfig({
       //   ],
       // },
     ],
-    // sidebar: [
-    //   {
-    //     text: 'Guide',
-    //     items: [
-    //       { text: 'Introduction', link: '/introduction' },
-    //       { text: 'Getting Started', link: '/getting-started' },
-    //     ],
-    //   },
-    // ],
+    sidebar: [
+      {
+        text: 'beaver河狸组件库',
+        items: [{ text: '主题', link: '/beaver/color' }],
+        collapsed: true, // 是否可折叠
+      },
+    ],
   },
 })
